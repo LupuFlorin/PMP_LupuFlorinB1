d7160cc8267fbde981dcf4458008269d102a60d5
LupuFlorin <florinlupu18@gmail.com>
2022-09-30 13:28:02 +0300

    Update GaussianDistribution.py

--- Lab1/GaussianDistribution.py
+++ Lab1/GaussianDistribution.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-mu, sigma = 0, 0.1 # mean and standard deviation
+mu, sigma = 0, 0.1
 s = np.random.normal(mu, sigma, 1000)
 
 count, bins, ignored = plt.hist(s, 100, density=True)
